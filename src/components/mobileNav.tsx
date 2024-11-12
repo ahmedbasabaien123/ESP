bd00b5c67854927d65a04f79d8c7477fa9df40cf
ahmedbasabaien123 <145780121+ahmedbasabaien123@users.noreply.github.com>
2024-11-12 21:10:37 +0100

    Update mobileNav.tsx

--- src/components/mobileNav.tsx
+++ src/components/mobileNav.tsx
@@ -31,7 +31,7 @@ export function MobileNav() {
     <div className="relative py-2 text-4xl md:hidden">
       <nav className="my-4 flex items-center justify-between">
         <a href="/">
-          <p className="text-2xl font-bold">Logo</p>
+          <p className="text-2xl font-bold"></p>
         </a>
         <div className="flex items-center gap-2">
           <NavigationMenu>
